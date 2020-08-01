422ebfb701c60e68ad1e052d1e01e93948401a18
Victor Elezua <victorelezua@gmail.com>
2020-08-01 09:07:03 +0100

    add blog urls

--- blogger/blogger/urls.py
+++ blogger/blogger/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import index_page, contact_page, about_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_page),
+    path('blog', include("blog.urls")),
     path('contact', contact_page),
     path('about', about_page),
 ]
