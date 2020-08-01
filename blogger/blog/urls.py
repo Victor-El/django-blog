3ec1298bfcde4eed4f2d1dddda6a7d4ad7728298
Victor Elezua <victorelezua@gmail.com>
2020-08-01 20:14:24 +0100

    add url entry for update view for article

--- blogger/blog/urls.py
+++ blogger/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", BlogIndex.as_view()),
     path("<int:pk>", ArticleDetail.as_view(), name="article_detail"),
     path("create", CreateArticle.as_view()),
+    path("update", CreateArticle.as_view()),
 ]
