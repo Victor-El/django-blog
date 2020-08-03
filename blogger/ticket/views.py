394210a0ad3253eb8b6659446a2cebd796385894
Victor Elezua <victorelezua@gmail.com>
2020-08-03 17:06:18 +0100

    create view to handle ticket submission

--- blogger/ticket/views.py
+++ blogger/ticket/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+
+from .models import Ticket
 
 # Create your views here.
+
+def submit_ticket(request, *args, **kwargs):
+    if request.method == "POST":
+        name = request.POST['name']
+        title = request.POST['title']
+        message = request.POST['message']
+
+        Ticket.objects.create(name=name, title=title, message=message)
+        return redirect('/ticket_success')
+
